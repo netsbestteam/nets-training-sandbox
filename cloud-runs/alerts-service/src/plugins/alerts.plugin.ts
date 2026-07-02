4de41696016d141c497d02e9239c7f951aa52d79
HHasidim <randommail200625@gmail.com>
2026-07-02 13:39:25 +0300

    feat: Add logger

--- cloud-runs/alerts-service/src/plugins/alerts.plugin.ts
+++ cloud-runs/alerts-service/src/plugins/alerts.plugin.ts
@@ -6,33 +6,53 @@ import {
   alerts,
 } from "../../../../shared-backend/src/db/schema";
 import { eq } from "drizzle-orm";
+import { logger } from "../../../../shared-backend/src/logger";
 
 export const alertRoutes = new Elysia({ prefix: "/alerts" })
   .get("/", async () => {
-    const result = await db.select().from(alerts);
+    try {
+      logger.info("Getting alerts");
+      const result = await db.select().from(alerts);
 
-    return result;
+      return result;
+    } catch (e: unknown) {
+      logger.error("Error getting alerts: " + e);
+      throw e;
+    }
   })
   .post(
     "/",
     async ({ body }) => {
-      await db.insert(alerts).values(body);
+      try {
+        logger.info("Attempting POST new alert");
+        await db.insert(alerts).values(body);
 
-      return {
-        received: body,
-      };
+        return {
+          received: body,
+        };
+      } catch (e: unknown) {
+        logger.error("Error POST new alert: " + e);
+        throw e;
+      }
     },
     { body: AlertSchema },
   )
   .patch(
     "/:id/status",
     async ({ params, body }) => {
-      await db
-        .update(alerts)
-        .set({ status: body.status })
-        .where(eq(alerts.alert_id, params.id));
+      try {
+        logger.info("Attempting PATCH alert status...");
 
-      return { status: body.status };
+        await db
+          .update(alerts)
+          .set({ status: body.status })
+          .where(eq(alerts.alert_id, params.id));
+
+        return { status: body.status };
+      } catch (e: unknown) {
+        logger.error("Error PATCH alert status: " + e);
+        throw e;
+      }
     },
     {
       body: t.Object({
@@ -43,16 +63,23 @@ export const alertRoutes = new Elysia({ prefix: "/alerts" })
   .post(
     "/:id/assign",
     async ({ params, body }) => {
-      await db.insert(alert_assignments).values({
-        investigatorId: body.investigatorId,
-        alertId: params.id,
-      });
+      logger.info("Attempting POST new alert assignment...");
 
-      return { data: body };
+      try {
+        await db.insert(alert_assignments).values({
+          investigatorId: body.investigatorId,
+          alertId: params.id,
+        });
+
+        return { data: body };
+      } catch (e: unknown) {
+        logger.error("Error POST new alert assignment: " + e);
+        throw e;
+      }
     },
     {
       body: t.Object({
-        investigatorId: t.String(),
+        investigatorId: t.String({ format: "uuid" }),
       }),
     },
   );
