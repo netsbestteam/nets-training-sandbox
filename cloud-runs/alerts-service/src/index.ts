5a1d893c6f27289a004754c41f3035125bea4a13
HHasidim <randommail200625@gmail.com>
2026-07-07 09:23:53 +0300

    fix: Place port to .env

--- cloud-runs/alerts-service/src/index.ts
+++ cloud-runs/alerts-service/src/index.ts
@@ -3,12 +3,14 @@ import { authGuard } from "./guards/auth.guard";
 import { alertRoutes } from "./plugins/alerts.plugin";
 import { UnauthorizedError } from "./errors/errors";
 import { connection } from "./plugins/connection.plugin";
+import { logger } from "../../../shared-backend/src/logger";
 
 export const app = new Elysia()
   .use(authGuard)
   .error({ UnauthorizedError })
   .onError(({ code, error, set }) => {
     if (code == "UnauthorizedError") {
+      logger.error("Unauthorized access");
       set.status = 401;
 
       return {
@@ -18,6 +20,7 @@ export const app = new Elysia()
       };
     }
 
+    logger.error("Error: " + error);
     return {
       status: "error",
       type: "INTERNAL_SERVER_ERROR",
@@ -26,11 +29,4 @@ export const app = new Elysia()
   })
   .use(connection)
   .use(alertRoutes)
-  .listen(3001);
-
-console.log(
-  app.routes.map((r) => ({
-    method: r.method,
-    path: r.path,
-  })),
-);
+  .listen(process.env.ALERT_SERVICE_PORT!);
